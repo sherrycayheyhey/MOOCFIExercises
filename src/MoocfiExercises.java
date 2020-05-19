77a05ecd90cfa265824b81c87b799293bdd5bc3d
sherrycayheyhey <sherrycaywood@yahoo.com>
2020-05-18 22:42:30 -0700

    Initial commit

---
+++ src/MoocfiExercises.java
@@ -0,0 +1,27 @@
+public class MoocfiExercises {
+    public static void main(String[] args) {
+
+        /*
+         *       ********PART 1********
+         */
+
+        //Printing
+
+        //01-02. Ada Lovelace
+        System.out.println("Ada Lovelace");
+
+        //01-03. Once Upon a Time
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a program");
+
+        //01-04. Dinosaur
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a dinosaur");
+
+        //
+    }
+}
+
+
