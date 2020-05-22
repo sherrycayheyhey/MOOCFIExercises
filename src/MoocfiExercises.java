63652ba81d112cf2bff6f0da7e9b72c5c15c8716
sherrycayheyhey <sherrycaywood@yahoo.com>
2020-05-21 22:39:27 -0700

    Calculating with Numbers section complete

--- src/MoocfiExercises.java
+++ src/MoocfiExercises.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class MoocfiExercises {
     public static void main(String[] args) {
 
@@ -91,7 +89,7 @@ public class MoocfiExercises {
         System.out.println("Perhaps " + characterName + " will not be " + job + " forever.");*/
 
 
-        //****************Variables****************
+        /*//****************Variables****************
 
         //****01-11 Various Variables****
         int numberOfChicken = 9000;
@@ -104,7 +102,7 @@ public class MoocfiExercises {
         System.out.println(baconWeight);
         System.out.println("Tractor:");
         System.out.println(tractor);
-        System.out.println("");
+        System.out.println();
         System.out.println("And finally, a summary:");
         System.out.println(numberOfChicken);
         System.out.println(baconWeight);
@@ -165,7 +163,119 @@ public class MoocfiExercises {
         System.out.println("You gave the string " + userString);
         System.out.println("You gave the integer " + userInt);
         System.out.println("You gave the double " + userDouble2);
-        System.out.println("You gave the boolean " + userBool2);
+        System.out.println("You gave the boolean " + userBool2);*/
+
+
+        /*//****************Variables****************
+
+        //****01-16 Seconds in a Day****
+        //create the scanner and prompt the user
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many days would you like to convert to seconds?");
+
+        //calculate the seconds and print
+        int days = Integer.parseInt(scanner.nextLine());
+        System.out.println(days * 86400);
+
+
+        //****01-17 Sum of Two Numbers****
+        //create the scanner, prompt the user for two numbers
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int first = Integer.parseInt(scanner2.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.parseInt(scanner2.nextLine());
+
+        // add the numbers and print the result
+        System.out.println("The sum of the numbers is " + (first + second));
+
+
+        //****01-18 Sum of Three Numbers****
+        //create the scanner and prompt the user for 3 numbers
+        Scanner scanner3 = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int first2 = Integer.parseInt(scanner3.nextLine());
+        System.out.println("Give the second number:");
+        int second2 = Integer.parseInt(scanner3.nextLine());
+        System.out.println("Give the third number:");
+        int third = Integer.parseInt(scanner3.nextLine());
+
+        //add the numbers and print the result
+        System.out.println("The sum of the numbers is " + (first2 + second2 + third));
+
+
+        //****01-19 Addition Formula****
+        //create the scanner and promp the user for 2 numbers
+        Scanner scanner4 = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int firstNum = Integer.parseInt(scanner4.nextLine());
+        System.out.println("Give the second number:");
+        int secondNum = Integer.parseInt(scanner4.nextLine());
+
+        //do the math and print out the equation with answer
+        System.out.println(firstNum + " + " + secondNum + " = " + (firstNum + secondNum));
+
+
+        //****01-20 Multiplication Formula****
+        //create the scanner and prompt the user for 2 numbers
+        Scanner scanner5 = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int firstNum2 = Integer.parseInt(scanner5.nextLine());
+        System.out.println("Give the second number:");
+        int secondNum2 = Integer.parseInt(scanner5.nextLine());
+
+        //print the equation with the answer
+        System.out.println(firstNum2 + " * " + secondNum2 + " = " + (firstNum2 * secondNum2));
+
+
+        //****01-21 Average of Two Numbers****
+        //this program should also check that 0 division never happens
+        //create the scanner and prompt the user for 2 numbers
+        Scanner scanner6 = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int firstNum3 = Integer.parseInt(scanner6.nextLine());
+        System.out.println("Give the second number:");
+        int secondNum3 = Integer.parseInt(scanner6.nextLine());
+
+        //add the numbers and cast them to double so info isn't loss, do the math, print the result
+        double result = (double) firstNum3 + secondNum3;
+        System.out.println("The average is " + (result / 2));
+
+
+        //****01-22 Average of Three Numbers****
+        //create the scanner and prompt the user for 3 numbers
+        Scanner scanner7 = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int firstNum4 = Integer.parseInt(scanner7.nextLine());
+        System.out.println("Give the second number:");
+        int secondNum4 = Integer.parseInt(scanner7.nextLine());
+        System.out.println("Give the third number:");
+        int thirdNum4 = Integer.parseInt(scanner7.nextLine());
+
+        //cast the result to a double, do the math, print the result
+        double result2 = (double) firstNum4 + secondNum4 + thirdNum4;
+        System.out.println("The average is " + (result2 / 3));
+
+
+        //****01-23 Simple Calculator****
+        //create the scanner, prompt the user for 2 numbers
+        Scanner scanner8 = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int firstNum5 = Integer.parseInt(scanner8.nextLine());
+        System.out.println("Give the second number:");
+        int secondNum5 = Integer.parseInt(scanner8.nextLine());
+
+        //do the math and print
+        System.out.println(firstNum5 + " + " + secondNum5 + " = " + (firstNum5 + secondNum5));
+        System.out.println(firstNum5 + " - " + secondNum5 + " = " + (firstNum5 - secondNum5));
+        System.out.println(firstNum5 + " * " + secondNum5 + " = " + (firstNum5 * secondNum5));
+        //firstNum5 multiplied by 1.0 so it's cast to a double so the result isn't truncated
+        System.out.println(firstNum5 + " / " + secondNum5 + " = " + (1.0 * firstNum5 / secondNum5));*/
+
+
+        //****************Conditional Statements and Conditional Operation****************
+        //****01-24 Speeding Ticket****
+        
     }
 }
 
